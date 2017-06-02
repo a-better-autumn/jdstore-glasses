599999d219c35c3294edd06be84a0ec0443ebfd7
Autumn <abetterautumn@gmail.com>
2017-06-02 19:52:43 +0800

    cannot add item with quantity 0

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -11,11 +11,11 @@ class Cart < ApplicationRecord
 
   def total_price
    sum = 0
-     cart_items.each do |cart_item|
-       if cart_item.product.price.present?
-         sum  = cart_item.quantity * cart_item.product.price
-       end
+   cart_items.each do |cart_item|
+     if cart_item.product.price.present?
+       sum += cart_item.quantity * cart_item.product.price
      end
+   end
    sum
   end
 
