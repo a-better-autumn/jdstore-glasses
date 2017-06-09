645d38d57221186a87eac8006910d51528decc76
NinaWei1384 <NinaWei1384@foxmail.com>
2017-06-09 19:32:44 +0800

    figaro

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :category
   validates :title, presence: true
+
+  has_many :photos
+  accepts_nested_attributes_for :photos
 end
